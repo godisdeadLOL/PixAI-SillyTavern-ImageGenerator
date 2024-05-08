e550031b9098317f99d6d05ff95e8e320e4395cd
Your Name <you@example.com>
2024-05-08 13:26:56 +0300

    added images

--- init.bat
+++ init.bat
@@ -2,5 +2,8 @@ If Not Exist ".venv" (
 	pip install virtualenv
 	python -m venv .venv
 )
+If Not Exist "account.txt" (
+	copy NUL account.txt
+)
 Call .venv\Scripts\activate
 pip install -r requirements.txt
